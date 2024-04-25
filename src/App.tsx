f9293bf15c6e9cf05f375f655e2aae86dde223bd
fgugnin22 <fedorgugnin22@gmail.com>
2024-04-25 13:46:01 +0300

    redux scaffolding

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Provider } from "react-redux";
+import { store } from "./store";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Provider store={store}>
+      <div className="text-3xl text-center my-24 font-bold">App</div>
+    </Provider>
+  );
 };
 
 export default App;
---
+++ src/store/index.ts
@@ -0,0 +1,21 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { Api } from "./api.ts";
+
+export const store = configureStore({
+  reducer: {
+    [Api.reducerPath]: Api.reducer
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(Api.middleware)
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export type AppStore = typeof store;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+setupListeners(store.dispatch);
